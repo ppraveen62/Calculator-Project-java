800d2fc3fa449993066bb86699d1474ac4e491ca
praveen <ppraveen62.ppk@gmail.com>
2022-08-24 23:46:24 +0530

    added

--- calculator.java
+++ calculator.java
@@ -4,8 +4,10 @@ import java.util.Scanner;
 
 public class calculator {
 	
-	public static void main(String[] args) {
+	
+	static void method() {
 		Scanner sc=new Scanner(System.in);
+		System.out.println();
 		System.out.println("give operator type ( + , - , / , % , *)?");
 		String operator = sc.next();
 		System.out.println("input first number:");
@@ -28,13 +30,36 @@ public class calculator {
 			System.out.println(a+"*"+b+"= "+(double)(a*b));
 			break;
 		case "%":
-			System.out.println(a+"%"+b+"= "+(double)(a%b));
+			System.out.println("result "+a+"%"+b+"= "+(double)(a%b));
 			break;
-		
+	
 		default:
 			System.out.println("error: wrong input");
 			break;
 		}
 	}
-
+		
+		public static void main(String[] args) {
+			Scanner scr=new Scanner(System.in);
+			System.out.println("calculator");
+			
+			while(true) {
+				method();
+				System.out.println();
+				System.out.println("type 0 (quit) || any (1-9) continue !");
+				int res =scr.nextInt();
+				if (res==0) {
+					break;
+				}
+				if (res!=0 ) {
+					System.out.println("...");
+					
+				}
+				
+				
+				
+			}
+			
+			
+	}
 }
